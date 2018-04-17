46462bf95e0cfe53a825911655d305f44f9eee5c
Theo Bouche <to.bouc@gmail.com>
2018-04-17 15:24:24 +0200

    Ajout de la comparaison de routes

--- src/Route.cpp
+++ src/Route.cpp
@@ -2,15 +2,11 @@
 
 bool Route::operator==(const Route & r) const {
 
-    // TODO
-
-    return false;
+  return villeA_==r.villeA_ and villeB_==r.villeB_ and distance_==r.distance_;
 }
 
 bool Route::operator!=(const Route & r) const {
 
-    // TODO
-
-    return false;
+  return !(operator==(r));
 }
 
